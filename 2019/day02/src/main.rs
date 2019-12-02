cf323d67077a4a0cf227eccf7a2fdda68f2cdaf4
nicoambrosini <nico.ambrosini14@gmail.com>
2019-12-02 16:12:26 +0100

    Fixed readme links

--- 2019/day02/src/main.rs
+++ 2019/day02/src/main.rs
@@ -4,13 +4,13 @@ use day02;
 
 fn main() {
     let filename = "input.txt";
-    let content: String = fs::read_to_string(filename).unwrap();
-    let numbers: Vec<i32> = content
+    let numbers: Vec<i32> = fs::read_to_string(filename).unwrap()
         .split(',')
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
 
     let mut numbers_copy: Vec<i32> = Vec::from(&numbers[..]);
+
     println!(
         "Day2 part 1: {}",
         day02::execute_with_fixed_state(12, 2, &mut numbers_copy)
