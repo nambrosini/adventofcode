13e88bace786b397c48df0b090562dcb0e68c618
nicoambrosini <nico.ambrosini14@gmail.com>
2019-12-04 08:41:08 +0100

    Solved day 4

--- 2019/day04/src/lib.rs
+++ 2019/day04/src/lib.rs
@@ -1,9 +1,92 @@
+pub fn solve_part_1(start: u32, end: u32) -> u32 {
+    let mut vec = vec![];
+    vec.extend(start..=end);
+
+    vec.iter()
+        .filter(|x| {
+            let x = convert_to_vec(**x);
+
+            check_double_digit(&x) && check_increasing(&x)
+        })
+        .count() as u32
+}
+
+pub fn solve_part_2(start: u32, end: u32) -> u32 {
+    let mut vec = vec![];
+    vec.extend(start..=end);
+
+    vec.iter()
+        .filter(|x| {
+            let x = convert_to_vec(**x);
+
+            check_double_digit_not_grouped(&x) && check_increasing(&x)
+        })
+        .count() as u32
+}
+
+fn convert_to_vec(password: u32) -> Vec<u32> {
+    password
+        .to_string()
+        .chars()
+        .map(|c| c.to_string().parse().unwrap())
+        .collect()
+}
+
+fn check_double_digit(password: &[u32]) -> bool {
+    for i in 0..password.len() - 1 {
+        if password[i] == password[i + 1] {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn check_double_digit_not_grouped(password: &[u32]) -> bool {
+    let mut count = 0;
+    let mut last = 10;
+
+    for i in password {
+        if *i == last {
+            count += 1;
+        } else {
+            if count == 2 {
+                return true;
+            }
+            last = *i;
+            count = 1;
+        }
+    }
+
+    count == 2
+}
+
+fn check_increasing(password: &[u32]) -> bool {
+    for i in 0..password.len() - 1 {
+        if password[i] > password[i + 1] {
+            return false;
+        }
+    }
+
+    true
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test() {
-        
+    fn test_one() {
+        assert_eq!(solve_part_1(111111, 111111), 1);
+        assert_eq!(solve_part_1(223450, 223450), 0);
+        assert_eq!(solve_part_1(123789, 123789), 0);
+    }
+
+    #[test]
+    fn test_two() {
+        assert_eq!(solve_part_2(112233, 112233), 1);
+        assert_eq!(solve_part_2(123444, 123444), 0);
+        assert_eq!(solve_part_2(111122, 111122), 1);
+        assert_eq!(solve_part_2(111112, 111112), 0);
+        assert!(check_double_digit_not_grouped(&vec![1, 1, 2, 2, 3, 3]));
     }
 }
--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -1,5 +1,13 @@
 use std::fs;
 
+use day04;
+
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
+
+    let start: u32 = input[..6].parse().unwrap();
+    let end: u32 = input[7..].parse().unwrap();
+
+    println!("Part 1: {}", day04::solve_part_1(start, end));
+    println!("Part 2: {}", day04::solve_part_2(start, end));
 }
