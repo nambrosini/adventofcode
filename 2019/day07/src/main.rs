37efb50dfacb2aa8949d9666bd2bc39fb92a792c
nicoambrosini <nico.ambrosini14@gmail.com>
2019-12-08 22:02:57 +0100

    Done day 7 and 8

---
+++ 2019/day07/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+use day07;
+
+fn main() {
+    let filename = "input.in";
+    let input = [1, 2, 3, 4, 5];
+
+    println!("Part 1: {}", day07::solve_part_1());
+
+    println!("Part 2: {}", day07::solve_part_2());
+}
