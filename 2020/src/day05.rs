d950e2f813ab441d1b05013a25e163b16a7a123d
nambrosini <nico.ambrosini14@gmail.com>
2020-12-11 17:51:16 +0100

    Done 2020 - day05

---
+++ 2020/src/day05.rs
@@ -0,0 +1,85 @@
+#[aoc_generator(day05)]
+pub fn generator(input: &str) -> Vec<usize> {
+    input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .map(|l| calc_id(&l))
+        .collect()
+}
+
+pub fn calc_id(id: &[char]) -> usize {
+    let (mut start_row, mut end_row) = (0, 127);
+    for i in 0..7 {
+        let c = id[i];
+
+        match c {
+            'F' => end_row -= (end_row - start_row + 1) / 2,
+            'B' => start_row += (end_row - start_row + 1) / 2,
+            _ => unreachable!()
+        }
+    }
+
+    let row = if id[6] == 'F' {
+        end_row
+    } else {
+        start_row
+    };
+
+    let (mut start_col, mut end_col) = (0, 7);
+
+    for i in 7..id.len() {
+        let c = id[i];
+
+        match c {
+            'L' => end_col -= (end_col - start_col + 1) / 2,
+            'R' => start_col += (end_col - start_col + 1) / 2,
+            _ => unreachable!()
+        }
+    }
+
+    let col = if id[id.len() - 1] == 'L' {
+        end_col
+    } else {
+        start_col
+    };
+
+    row * 8 + col
+}
+
+#[aoc(day05, part1)]
+pub fn part1(input: &[usize]) -> usize {
+    *input.iter()
+        .max()
+        .unwrap()
+}
+
+#[aoc(day5, part2)]
+pub fn part2(input: &[usize]) -> usize {
+    let min = input.iter().min().unwrap().clone();
+    let max = input.iter().max().unwrap().clone();
+
+    (min..=max)
+        .filter(|x| !input.contains(x))
+        .next()
+        .unwrap()
+        .clone()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1_part1() {
+        let s = generator("FBFBBFFRLR\nBFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL");
+
+        assert_eq!(part1(&s), 820);
+    }
+
+    #[test]
+    fn sample1_part2() {
+        let s = generator("FBFBBFFRLR\nBFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL");
+
+        assert_eq!(part2(&s), 120);
+    }
+}
--- 2020/src/lib.rs
+++ 2020/src/lib.rs
@@ -10,6 +10,7 @@ pub mod day01;
 pub mod day02;
 pub mod day03;
 pub mod day04;
+pub mod day05;
 pub mod day10;
 pub mod day11;
 
