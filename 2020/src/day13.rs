c95942352f8002ee84819ae73d6b281b257ad208
nambrosini <nico.ambrosini14@gmail.com>
2020-12-14 10:31:18 +0100

    Done 2020 - day14

---
+++ 2020/src/day13.rs
@@ -0,0 +1,111 @@
+type Lines = (usize, Vec<usize>);
+
+fn egcd(a: i128, b: i128) -> (i128, i128, i128) {
+    if a == 0 {
+        (b, 0, 1)
+    } else {
+        let (g, x, y) = egcd(b % a, a);
+        (g, y - (b / a) * x, x)
+    }
+}
+
+fn mod_inv(x: i128, n: i128) -> i128 {
+    let (g, x, _) = egcd(x, n);
+    if g == 1 {
+        (x % n + n) % n
+    } else {
+        0
+    }
+}
+
+#[aoc_generator(day13, part1)]
+fn generator_part1(input: &str) -> Lines {
+    let split: Vec<_> = input.lines().collect();
+
+    let time: usize = split[0].parse().unwrap();
+
+    let mut times: Vec<usize> = vec![];
+
+    for s in split[1].split(",") {
+        let time = s.parse();
+
+        if let Ok(v) = time {
+            times.push(v);
+        }
+    }
+
+    (time, times)
+}
+
+#[aoc_generator(day13, part2)]
+fn generator_part2(input: &str) -> Vec<(i128, i128)> {
+    let lines = input.lines();
+
+    lines
+        .last()
+        .unwrap()
+        .trim()
+        .split(',')
+        .enumerate()
+        .filter(|(_, x)| *x != "x")
+        .map(|(i, x)| {
+            let num = x.parse::<i128>().unwrap();
+            return ((num - (i as i128 % num)) % num, num);
+        })
+        .collect()
+}
+
+#[aoc(day13, part1)]
+pub fn part1(input: &Lines) -> usize {
+    let next_departing = input.1.iter()
+        .enumerate()
+        .map(|(i, x)| (i, input.0 + x - (input.0 % x)))
+        .min_by_key(|(_, x)| x.clone())
+        .unwrap();
+
+    (next_departing.1 - input.0) * input.1[next_departing.0]
+}
+
+#[aoc(day13, part2)]
+pub fn part2(input: &[(i128, i128)]) -> i128 {
+    let n: i128 = input
+        .iter()
+        .map(|(_, n_i)| n_i)
+        .product();
+    
+    let x: i128 = input
+        .iter()
+        .map(|(a_i, n_i)| {
+            let tmp = n / n_i;   
+            return a_i * mod_inv(tmp, *n_i) * tmp;
+        })
+        .sum();
+
+    return x % n;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        let s = generator_part1("939\n7,13,x,x,59,x,31,19");
+
+        assert_eq!(part1(&s), 295);
+    }
+
+    #[test]
+    fn sample2() {
+        let s = generator_part2("939\n17,x,13,19");
+
+        assert_eq!(part2(&s), 3417);
+    }
+
+    #[test]
+    fn sample3() {
+        let s = generator_part2("939\n67,7,59,61");
+
+        assert_eq!(part2(&s), 754018);
+    }
+}
---
+++ 2020/src/day14.rs
@@ -0,0 +1,182 @@
+use std::collections::{HashMap, HashSet};
+
+#[aoc_generator(day14)]
+fn generator(input: &str) -> Vec<String> {
+    input.lines()
+        .map(|l| l.to_owned())
+        .collect()
+}
+
+fn get_masked_value(mask: &str, value: i64) -> i64 {
+    let mut bits: Vec<i64> = vec![0; mask.len()];
+    let mut v = value.clone();
+    let mut counter = bits.len() - 1;
+
+    while v > 0 {
+        let bit = v & 1;
+        bits[counter] = bit;
+        v = v >> 1;
+        counter -= 1;
+    }
+
+    let mask: Vec<char> = mask.chars().collect();
+
+    for i in 0..mask.len() {
+        if mask[i] == '1' {
+            bits[i] = 1;
+        } else if mask[i] == '0' {
+            bits[i] = 0;
+        }
+    }
+
+    let mut res: i64 = 0;
+    let min = bits.iter()
+        .enumerate()
+        .filter(|(_, &x)| x == 1)
+        .next()
+        .unwrap().0;
+
+    for i in min..bits.len() {
+        res = res << 1;
+        res += bits[i];
+    }
+    res
+}
+
+fn get_addresses(mask: &str, value: i64) -> Vec<i64> {
+    let mut bits: Vec<char> = vec!['0'; mask.len()];
+    let mut v = value.clone();
+    let mut counter = bits.len() - 1;
+
+    while v > 0 {
+        let bit = v & 1;
+        bits[counter] = if bit == 0 {
+            '0'
+        } else {
+            '1'
+        };
+        v = v >> 1;
+        counter -= 1;
+    }
+
+    let mask: Vec<char> = mask.chars().collect();
+
+    for i in 0..mask.len() {
+        if mask[i] != '0'{
+            bits[i] = mask[i];
+        }
+    }
+
+    let bits_x_pos: Vec<usize> = bits.iter()
+        .enumerate()
+        .filter(|(_, &x)| x == 'X')
+        .map(|(i, _)| i)
+        .collect();
+
+    let mut bits_results: Vec<Vec<char>> = vec![];
+
+    for i in 0..=(2usize.pow(bits_x_pos.len() as u32)) {
+        let mut b = bits.clone();
+
+        for j in 0..bits_x_pos.len() {
+            let v = (i >> j) & 1;
+            let v = if v == 0 {
+                '0'
+            } else {
+                '1'
+            };
+
+            b[bits_x_pos[j as usize] as usize] = v;
+        }
+
+        bits_results.push(b.clone());
+    }
+
+    let mut results: Vec<i64> = vec![];
+
+    for b in bits_results {
+        let mut res = 0;
+        let min = b.iter()
+            .enumerate()
+            .filter(|(_, &x)| x == '1')
+            .next()
+            .unwrap()
+            .0;
+        for i in min..bits.len() {
+            res = res << 1;
+            res += if b[i] == '1' {
+                1
+            } else {
+                0
+            };
+        }
+
+        results.push(res);
+    }
+
+    results
+}
+
+#[aoc(day14, part1)]
+fn part1(input: &[String]) -> i64 {
+    let mut mem: HashMap<usize, i64> = HashMap::new();
+    let mut mask = "";
+
+    for l in input {
+        let split: Vec<&str> = l.split(" = ").collect();
+
+        if split[0] == "mask" {
+            mask = split[1];
+        } else {
+            let index = split[0];
+            let index: usize = index[4..index.len() - 1].parse().unwrap();
+            let value: i64 = split[1].parse().unwrap();
+            let m = mem.entry(index).or_insert(0);
+            *m = get_masked_value(mask, value);
+        }
+    }
+
+    mem.values().sum()
+}
+
+#[aoc(day14, part2)]
+fn part2(input: &[String]) -> u64 {
+    let mut mem: HashMap<usize, u64> = HashMap::new();
+    let mut mask = "";
+
+    for l in input {
+        let split: Vec<&str> = l.split(" = ").collect();
+
+        if split[0] == "mask" {
+            mask = split[1];
+        } else {
+            let address: i64 = split[0][4..split[0].len() - 1].parse().unwrap();
+            let value: u64 = split[1].parse().unwrap(); 
+            let address = get_addresses(mask, address);
+            for x in address {
+                mem.insert(x as usize, value);
+            }
+        }
+    }
+
+    mem.values().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        let s = generator("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\nmem[8] = 11\nmem[7] = 101\nmem[8] = 0");
+
+        assert_eq!(part1(&s), 165);
+    }
+
+    #[test]
+    fn sample2() {
+        let s = generator("mask = 000000000000000000000000000000X1001X\nmem[42] = 100\nmask = 00000000000000000000000000000000X0XX\nmem[26] = 1");
+
+        assert_eq!(part2(&s), 208);
+    }
+}
