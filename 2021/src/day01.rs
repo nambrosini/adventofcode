c6dd6aa2a2533a6a4060a152037427b03133693d
nambrosini <nico.ambrosini@icloud.com>
2021-12-01 06:34:04 +0100

    Done 2021 - day1

---
+++ 2021/src/day01.rs
@@ -0,0 +1,24 @@
+use itertools::Itertools;
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<usize> {
+    input.lines().map(|x| x.parse().unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &[usize]) -> usize {
+    input.windows(2)
+        .filter(|x| x[1] > x[0])
+        .count()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &[usize]) -> usize {
+    input.windows(3)
+        .map(|x| x.iter().sum::<usize>())
+        .collect_vec()
+        .windows(2)
+        .filter(|x| x[1] > x[0])
+        .count()
+    
+}
---
+++ 2021/src/lib.rs
@@ -0,0 +1,14 @@
+extern crate aoc_runner;
+extern crate evalexpr;
+extern crate itertools;
+extern crate pathfinding;
+extern crate primes;
+extern crate regex;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+extern crate lazy_static;
+
+pub mod day01;
+
+aoc_lib! { year = 2021 }
