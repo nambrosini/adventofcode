7b930b38b73aeedb60e97626115e9484b1cafd2a
nambrosini <nico.ambrosini@icloud.com>
2021-12-07 06:09:45 +0100

    Done day07

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -1,19 +1,45 @@
 use itertools::Itertools;
 
 #[aoc_generator(day07)]
-pub fn generator(input: &str) -> Vec<_> {
+pub fn generator(input: &str) -> Vec<i32> {
     input
-        .lines()
-        .map(|line| line)
+        .split(',')
+        .map(|line| line.parse().unwrap())
         .collect_vec()
 }
 
 #[aoc(day07, part1)]
-pub fn part1(input: &[_]) -> usize {
-    0
+pub fn part1(input: &[i32]) -> i32 {
+    let max = *input.iter().max().unwrap();
+
+    let mut min: i32 = i32::MAX;
+
+    for i in 0..max {
+        let sum = input.iter().map(|x| (x - i).abs()).sum::<i32>();
+        if sum < min {
+            min = sum;
+        }
+    }
+
+    min
 }
 
 #[aoc(day07, part2)]
-pub fn part2(input: &[_]) -> usize {
-    0
+pub fn part2(input: &[i32]) -> i32 {
+    let max = *input.iter().max().unwrap();
+
+    let mut min: i32 = i32::MAX;
+
+    for i in 0..max {
+        let sum = input.iter().map(|x| sum_prev((x - i).abs())).sum::<i32>();
+        if sum < min {
+            min = sum;
+        }
+    }
+
+    min
+}
+
+fn sum_prev(x: i32) -> i32 {
+    (x / 2) * (1 + x)
 }
