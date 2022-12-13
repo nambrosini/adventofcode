4775342a7ec9861d24696d056cafaa42b703390e
Ambrosini Nico (IT-PTR-CEN1-BDE4) <nico.ambrosini@sbb.ch>
2022-12-13 09:48:08 +0100

    2022/Updated day12

--- 2022/src/day12.rs
+++ 2022/src/day12.rs
@@ -1,15 +1,14 @@
-use itertools::Itertools;
-use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 use std::usize;
 
 #[aoc_generator(day12)]
-pub fn generator(input: &str) -> Vec<Vec<char>> {
+pub fn generator(input: &str) -> Grid<char> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
 #[aoc(day12, part1)]
-pub fn part1(input: &[Vec<char>]) -> usize {
+pub fn part1(input: &Grid<char>) -> usize {
     let start = find_character(input, 'S')[0];
     let end = find_character(input, 'E')[0];
     let mut input = input.to_vec();
@@ -17,11 +16,11 @@ pub fn part1(input: &[Vec<char>]) -> usize {
     input[start.0 as usize][start.1 as usize] = 'a';
     input[end.0 as usize][end.1 as usize] = 'z';
 
-    astar(&input, &start, &end).len() - 1
+    a_star(&input, &start, &end, heuristics, neighbors).unwrap().len() - 1
 }
 
 #[aoc(day12, part2)]
-pub fn part2(input: &[Vec<char>]) -> usize {
+pub fn part2(input: &Grid<char>) -> usize {
     let mut start = find_character(input, 'a');
     start.append(&mut find_character(input, 'S'));
 
@@ -34,14 +33,28 @@ pub fn part2(input: &[Vec<char>]) -> usize {
 
     start
         .iter()
-        .map(|start| astar(&input, start, &end).len())
-        .filter(|res| res != &0)
+        .map(|start|a_star(&input, start, &end, heuristics, neighbors).map(|path| path.len()))
+        .filter(|res| res.is_some())
         .min()
+        .unwrap()
         .unwrap()
         - 1
 }
 
-fn find_character(map: &[Vec<char>], searched: char) -> Vec<Point> {
+fn heuristics(current: &Position, end: &Position) -> i32 {
+    ((end.0 - current.0).pow(2) as f64 + (end.1 - current.1).pow(2) as f64).sqrt() as i32
+}
+
+fn neighbors(grid: &Grid<char>, current: &Position) -> Vec<Position> {
+    let v = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+    v.iter()
+        .map(|p| (p.0 + current.0, p.1 + current.1))
+        .filter(|p| (0..grid.len() as i32).contains(&p.0) && (0..grid[0].len() as i32).contains(&p.1))
+        .filter(|p| grid[current.0 as usize][current.1 as usize] as u8 + 1 >= grid[p.0 as usize][p.1 as usize] as u8)
+        .collect()
+}
+
+fn find_character(map: &Grid<char>, searched: char) -> Vec<Position> {
     map.iter()
         .flatten()
         .enumerate()
@@ -55,116 +68,112 @@ fn find_character(map: &[Vec<char>], searched: char) -> Vec<Point> {
         .collect()
 }
 
-type Point = (i32, i32);
+pub type Grid<T> = Vec<Vec<T>>;
+pub type Position = (i32, i32);
 
-pub fn astar(map: &[Vec<char>], start: &Point, end: &Point) -> Vec<Point> {
-    let mut open_list: HashSet<Node> = HashSet::new();
-    let mut closed_list: HashSet<Node> = HashSet::new();
-
-    let start = Node::new(*start, None);
-    let end = Node::new(*end, None);
-
-    open_list.insert(start);
-
-    while !open_list.is_empty() {
-        let current_node = *open_list.clone().iter().min_by_key(|n| n.f).unwrap();
-
-        open_list.remove(&current_node);
-        closed_list.insert(current_node);
-
-        if current_node == end {
-            let mut path = vec![];
-            let mut current = Some(&current_node);
-            while let Some(c) = current {
-                current = closed_list.iter().find(|x| Some(x.position) == c.parent);
-                path.push(c.position);
-            }
-            return path.iter().rev().cloned().collect::<Vec<Point>>();
-        }
-
-        let children = get_neighbours(&current_node);
-
-        for child in children {
-            if !(0..map.len() as i32).contains(&child.position.0)
-                || !(0..map[0].len() as i32).contains(&child.position.1)
-            {
-                continue;
-            }
-            let current_value =
-                map[current_node.position.0 as usize][current_node.position.1 as usize];
-            let child_value = map[child.position.0 as usize][child.position.1 as usize];
-            if current_value as u8 + 1 < child_value as u8 {
-                continue;
-            }
-            if closed_list.iter().any(|&c| child == c) {
-                continue;
-            }
-
-            let mut child = child;
-            child.g = current_node.g + 1;
-            child.h = ((end.position.0 - child.position.0).pow(2) as f64
-                + (end.position.1 - child.position.1).pow(2) as f64)
-                .sqrt() as i32;
-            child.f = child.g + child.h;
-
-            if open_list.iter().any(|&x| x == child && child.g > x.g) {
-                continue;
-            }
-
-            open_list.insert(child);
-        }
-    }
-
-    vec![]
-}
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone)]
 struct Node {
-    parent: Option<Point>,
-    position: Point,
-    f: i32,
+    parent: Option<Box<Node>>,
+    position: Position,
     g: i32,
     h: i32,
+    f: i32,
 }
 
 impl Node {
-    pub fn new(position: Point, parent: Option<Point>) -> Self {
+    fn new(position: Position, parent: Option<Box<Node>>) -> Self {
         Self {
             parent,
             position,
             g: 0,
             h: 0,
-            f: 0,
+            f: 0
         }
-    }
-}
-
-impl Hash for Node {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.position.hash(state);
-    }
-}
-
-impl PartialEq for Node {
-    fn eq(&self, other: &Self) -> bool {
-        self.position == other.position
     }
 }
 
 impl Eq for Node {}
 
-fn get_neighbours(current: &Node) -> Vec<Node> {
-    let neighbours = vec![(-1, 0), (0, -1), (0, 1), (1, 0)];
+impl PartialEq<Self> for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.f == other.f
+    }
+}
 
-    neighbours
-        .iter()
-        .map(|p| {
-            Node::new(
-                (current.position.0 + p.0, current.position.1 + p.1),
-                Some(current.position),
-            )
-        })
-        .collect_vec()
+impl PartialOrd<Self> for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(other.f.cmp(&self.f))
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+pub fn a_star<T>(
+    grid: &Grid<T>,
+    start: &Position,
+    end: &Position,
+    heuristics: fn(&Position, &Position) -> i32,
+    neighbors: fn(&Grid<T>, &Position) -> Vec<Position>,
+) -> Option<Vec<Position>> {
+    let mut open = BinaryHeap::new();
+    open.push(Node {
+        parent: None,
+        position: *start,
+        g: 0,
+        h: 0,
+        f: 0,
+    });
+    open.push(Node::new(*start, None));
+    let mut closed: Vec<Node> = vec![];
+
+    while let Some(current) = open.pop() {
+        if closed.iter().any(|n| n.position == current.position) {
+            continue;
+        }
+        closed.push(current.clone());
+
+        if &current.position == end {
+            let mut path: Vec<Position> = vec![];
+            let mut current: Option<Box<Node>> = Some(Box::new(current));
+
+            while let Some(n) = current {
+                path.insert(0, n.as_ref().position);
+                current = n.parent.clone();
+            }
+
+            return Some(path);
+        }
+
+        let neighbors = neighbors(grid, &current.position);
+
+        for child in neighbors {
+            let mut child = Node::new(child, Some(Box::new(current.clone())));
+            child.g = current.g + 1;
+            child.h = heuristics(&current.position, end);
+            child.f = child.g + child.h;
+
+            if open.iter().any(|n| n.position == child.position && child.g > n.g) {
+                continue;
+            }
+
+            if let Some(n) = closed.iter_mut().find(|node| node.position == child.position) {
+                if &child < n {
+                    n.parent = child.parent.clone();
+                    n.g = child.g;
+                    n.h = child.h;
+                    n.f = child.f;
+                }
+            }
+
+            open.push(child);
+        }
+    }
+
+    None
 }
 
 #[test]
